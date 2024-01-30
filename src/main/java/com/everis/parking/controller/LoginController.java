ca4c7d22f6e7d96ef5774b25fc98f7d38df1e1db
Cus <marcus.costa.cxs@gmail.com>
2024-01-30 00:14:59 -0500

    Add H2 database dependency and configuration, and update MySQL configuration

--- src/main/java/com/everis/parking/controller/LoginController.java
+++ src/main/java/com/everis/parking/controller/LoginController.java
@@ -14,12 +14,22 @@ import com.everis.parking.dto.LoginDto;
 import com.everis.parking.dto.LoginResponseDto;
 import com.everis.parking.service.LoginService;
 
+/**
+ * Controlador responsável por lidar com as requisições relacionadas ao login de usuários.
+ */
 @RestController
 @RequestMapping("/api")
 public class LoginController {
 	@Autowired 
 	LoginService loginService;
 	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+	
+	/**
+	 * Endpoint para realizar o login de um usuário.
+	 * 
+	 * @param loginDto O objeto contendo as informações de login do usuário.
+	 * @return A resposta contendo os dados do usuário logado.
+	 */
 	@PostMapping("/login")
 	public ResponseEntity<LoginResponseDto> login(@RequestBody LoginDto loginDto){
 		logger.info("dentro do método de login..");
